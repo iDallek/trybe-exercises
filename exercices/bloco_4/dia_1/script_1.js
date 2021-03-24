f106ec06060f76ee32862b91bc417cac8c8872b8
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-24 16:01:57 -0300

    Parte I exercicio finalizado

---
+++ exercices/bloco_4/dia_1/script_1.js
@@ -0,0 +1,13 @@
+const name = "Gabriel Carvalho Barbosa"
+const birthCity = "Goiânia"
+let birthYear = 2002
+
+console.log(`Nome: ${name}.\nCidade natal: ${birthCity}.\nAno de nascimento: ${birthYear}.\n`)
+
+birthYear -= 2
+
+console.log(`Nome: ${name}.\nCidade natal: ${birthCity}.\nAno de nascimento: ${birthYear}.\n`)
+
+birthCity = "Alubarna"
+
+console.log(birthCity)
