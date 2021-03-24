295f4597c9b9f5ca507f7cf9e835da744ae40a11
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-24 18:20:44 -0300

    Etapa 2 e 3 concluida

--- exercices/bloco_4/dia_1/part_IV/script.js
+++ exercices/bloco_4/dia_1/part_IV/script.js
@@ -1,1 +1,9 @@
 let grade = 27
+
+if(grade >= 80){
+  console.log('Parabéns, você foi aprovado(o)!')
+} else if (grade < 80 && grade === 60) {
+  console.log('Você está na nossa lista de espera.')
+} else {
+  console.log('Você foi reprovado(o).')
+}
