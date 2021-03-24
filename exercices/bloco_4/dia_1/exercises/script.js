6f8bc8b23077dfced09304fc08ebb60cbd61500a
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-24 19:12:53 -0300

    Exercicio 4.1, etapa 1 finalizado

---
+++ exercices/bloco_4/dia_1/exercises/script.js
@@ -0,0 +1,29 @@
+//Base do programa 1 ao 5
+
+let a = 27;
+let b = 54;
+
+//Programa I
+let soma;
+soma = a + b;
+console.log('Resultado da soma é',soma + '.');
+
+//Programa II
+let subtracao;
+subtracao = a - b;
+console.log('Resultado da subtração é:',subtracao + '.');
+
+//Programa III
+let multiplicacao;
+multiplicacao = a * b;
+console.log('Resultado da multiplicação é:',multiplicacao + '.');
+
+//Programa IV
+let divisao;
+divisao = a / b;
+console.log('Resultado da divisão é:',divisao + '.');
+
+//Programa V
+let modolo;
+modolo = a % b;
+console.log('Resultado da módolo é:',modolo + '.');
