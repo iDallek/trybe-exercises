5775b13b5ce8fae39f1c6c53b7f46aa647a34615
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-25 18:05:44 -0300

    Exercio etapa 2 finalizada

--- exercices/bloco_4/dia_2/exercicios/script.js
+++ exercices/bloco_4/dia_2/exercicios/script.js
@@ -1,3 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
+
+let arrayCount = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+  arrayCount += numbers[index];
+}
+
+console.log(arrayCount);
