f31722ba9dc80998913f3491754f6219f95947ba
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-25 19:00:54 -0300

    Exercicio etapa 6 finalizada

--- exercices/bloco_4/dia_2/exercicios/script.js
+++ exercices/bloco_4/dia_2/exercicios/script.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27,];
 
 console.log(numbers);
 
@@ -53,3 +53,18 @@ for(let index = 0; index < numbers.length; index += 1) {
 
 console.log('A quantidade de numeros primos são:', quantityOfPrimeNumbers);
 */
+
+let quantityOfOddNumbers = 0;
+
+for(let i = 0; i < numbers.length; i += 1){
+  let arrayNumber = numbers[i];
+  if(arrayNumber % 2 != 0){
+    quantityOfOddNumbers += 1
+  } else {}
+}
+
+if(quantityOfOddNumbers === 0){
+  console.log('Nenhum valor ímpar encontrado.')
+}
+
+console.log('A quantidade de números ímpares são:', quantityOfOddNumbers);
