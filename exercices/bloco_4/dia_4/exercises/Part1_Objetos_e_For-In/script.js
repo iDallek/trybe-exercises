a8027565a8e306e69b494348cf90c56a6abd1c14
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-29 18:32:47 -0300

    Etapa 5 finalizado

--- exercices/bloco_4/dia_4/exercises/Part1_Objetos_e_For-In/script.js
+++ exercices/bloco_4/dia_4/exercises/Part1_Objetos_e_For-In/script.js
@@ -12,7 +12,7 @@ console.log('1. Bem-vinda, ' + info.personagem + '.');
 
 info.recorrente = 'Sim';
 
-console.log('2.' + info);
+console.log(info);
 
 ////////////////////////////////////////////////////////
 
@@ -24,4 +24,17 @@ for(let key in info) {
 
 for(let key in info) {
   console.log('4.' + info[key])
+}
+
+////////////////////////////////////////////////////////
+
+let info2 = {
+  personagem: 'Margarida e Tio Patinhas',
+  origem: "Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas',
+  recorrente: 'Ambos recorrentes'
+}
+
+for(let key in info2) {
+  console.log('5.' + info2[key])
 }
