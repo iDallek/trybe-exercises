d7afef7fc490a68eb2c71beb2e79672804bd9df5
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-03-29 19:11:28 -0300

    Etapa 2 finalizado

--- exercices/bloco_4/dia_4/exercises/Part2_Funcoes/script.js
+++ exercices/bloco_4/dia_4/exercises/Part2_Funcoes/script.js
@@ -1,11 +1,7 @@
 function checkPalindrome(str) {
   let isPalindrome;
 
-  /**
-   * Consultei o Stack Overflow para saber como inverte uma string
-   * Source link: https://stackoverflow.com/a/959004
-   */
-
+  /** Source: https://stackoverflow.com/a/959004 */
   let splitStr = str.split('').reverse().join('').toLowerCase();
   
   if(str.toLowerCase() === splitStr) {
@@ -21,3 +17,15 @@ console.log(checkPalindrome('Aguia'))
 console.log(checkPalindrome('Arara'))
 
 ///////////////////////////////
+
+let arrayNumbers = [2, 6, 8, 10, 12, 3];
+
+function returnHighestValueIndex(numbers) {
+
+  /** Source: https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array/11301464 */  
+  let highestIndex = numbers.indexOf(Math.max(...numbers));
+  
+  return highestIndex
+}
+
+console.log(returnHighestValueIndex(arrayNumbers))
