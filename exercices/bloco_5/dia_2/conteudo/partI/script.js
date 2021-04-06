a85ab1648b498eb44e1562ac6eef055766b38a9b
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-04-06 15:49:14 -0300

    Exercicios partI finalizados

---
+++ exercices/bloco_5/dia_2/conteudo/partI/script.js
@@ -0,0 +1,13 @@
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'orange';
+
+document.querySelector('#primeiroFilhoDoFilho').innerHTML = '<p>GlaAlabastaIsComing</p>';
+
+console.log(document.querySelector('#pai').firstElementChild);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling.nextSibling);
+
+console.log(document.querySelector('#pai').children[2]);
