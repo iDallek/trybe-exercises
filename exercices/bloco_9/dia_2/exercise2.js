bce65f157c23458c37780a61a037bd8cf7982511
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-05-12 20:01:30 -0300

    Exercicio 2 finalizado

---
+++ exercices/bloco_9/dia_2/exercise2.js
@@ -0,0 +1,26 @@
+const getArrWith10RandomsNumbers = () => {
+  let arr = [];
+  let num = 0;
+  for (let index = 0; index < 10; index += 1) {
+    num = Math.floor(Math.random() * 50) + 1
+    arr.push(Math.pow(num, 2))
+  }
+  return arr;
+}
+
+const sumArrPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const arr = getArrWith10RandomsNumbers();
+    const sumArr = arr.reduce((a, b) => a + b, 0)
+
+    console.log(sumArr)
+
+    sumArr < 8000 ? resolve() : reject();
+  });
+
+  myPromise
+    .then(() => console.log('=) Promise resolvida'))
+    .catch(() => console.log('=( Promise rejeitada'));
+}
+
+sumArrPromise();
